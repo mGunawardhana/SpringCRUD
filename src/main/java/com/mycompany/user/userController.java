281811e083a37a60822fce5ed5280025f1d9f442
mGunawardhana <mrgunawardhana27368@gmail.com>
2023-01-12 23:44:10 +0530

    content update

--- src/main/java/com/mycompany/user/userController.java
+++ src/main/java/com/mycompany/user/userController.java
@@ -1,11 +1,24 @@
 package com.mycompany.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class userController {
 
+    @Autowired
+    private UserService service;
 
+    @GetMapping("/users")
+    public String showUserList(Model model) {
+        List<User> listUsers = service.listAll();
+        model.addAttribute("listUsers",listUsers);
+        return "users";
+    }
 
 }
 
